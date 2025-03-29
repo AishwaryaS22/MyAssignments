7f081f4414ddfb73fee255eeadebb0e5f2fa9a87
AishwaryaS22 <aishu22694@gmail.com>
2025-03-30 00:22:06 +0530

    Add files via upload

---
+++ day1/Assignment1.java
@@ -0,0 +1,10 @@
+package week1.day1;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Test Leaf");
+
+	}
+
+}
---
+++ day1/Chrome.java
@@ -0,0 +1,35 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		//Float
+		float version=91.0f;
+		
+		
+		//String
+		String developer="Google";
+		
+		
+		//boolean
+		boolean isBeta=false;
+		
+		//int
+		int releaseYear=2008;
+		
+		//character
+		char shortcutKey='C';
+		System.out.println(version);
+		System.out.println(developer);
+		System.out.println(isBeta);
+		System.out.println(releaseYear);
+		System.out.println(shortcutKey);
+		
+		
+		
+		
+		
+
+	}
+
+}
